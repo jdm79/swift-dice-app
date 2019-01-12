d4a2b7fd03fce1855c1a134c239d097a31a086ea
jdm79 <james.malvern@gmail.com>
2019-01-12 01:27:33 +0000

    feat: displays number of rolls, running amount and roll total, improves the layout

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -22,13 +22,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var totalAmountView: UITextField!
     @IBOutlet weak var previousAmountView: UITextField!
+    @IBOutlet weak var totalRollsView: UITextField!
     
     override func viewDidLoad() {
         // this is a bit like componentDidMount in React
         // only gets triggered when the view loads up
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        updateDiceImages()
+//        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
@@ -56,13 +57,16 @@ class ViewController: UIViewController {
         randomDiceIndex2 = Int.random(in: 0 ... 5);
         total = (randomDiceIndex1 + randomDiceIndex2) + 2;
         runningTotal = total + previousRoll[i];
+            
         print(i)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         totalAmountView.text = String(total)
         previousRoll.append(runningTotal);
+        totalRollsView.text = String(i + 1)
         previousAmountView.text = String(runningTotal)
+
         i = i + 1;
 
         
@@ -76,9 +80,12 @@ class ViewController: UIViewController {
         i = 0;
         totalAmountView.text = String(0)
         previousAmountView.text = String(0)
+        totalRollsView.text = String(0)
+
         // this makes no image appear for the di as dice6 is the last one
         diceImageView1.image = UIImage(named: "dice\(7)")
         diceImageView2.image = UIImage(named: "dice\(7)")
+
         
     }
     
