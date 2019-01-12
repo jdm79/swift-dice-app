6fdb5cf87aa4b86fc5eff43b3d5c6a32209e7842
jdm79 <james.malvern@gmail.com>
2019-01-12 00:41:48 +0000

    feat: adds a running total at the top, plus a clear button to reset everything

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"];
+    var previousRoll: [Int] = [0];
     var randomDiceIndex1 : Int = 0;
     var randomDiceIndex2 : Int = 0;
+    var i : Int = 0;
     var total : Int = 0;
+    var runningTotal : Int = 0;
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var totalAmountView: UITextField!
+    @IBOutlet weak var previousAmountView: UITextField!
     
     override func viewDidLoad() {
         // this is a bit like componentDidMount in React
@@ -33,6 +37,12 @@ class ViewController: UIViewController {
 
     }
     
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+    
+        clear()
+    
+    }
+    
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         
         updateDiceImages()
@@ -42,16 +52,31 @@ class ViewController: UIViewController {
     func updateDiceImages() {
         
         // change range to 1 ... 6 if using the dynamic image path below
-        randomDiceIndex1 = Int.random(in: 0 ... 5)
-        randomDiceIndex2 = Int.random(in: 0 ... 5)
-        total = (randomDiceIndex1 + randomDiceIndex2) + 2
+        randomDiceIndex1 = Int.random(in: 0 ... 5);
+        randomDiceIndex2 = Int.random(in: 0 ... 5);
+        total = (randomDiceIndex1 + randomDiceIndex2) + 2;
+        runningTotal = total + previousRoll[i];
+        print(i)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         totalAmountView.text = String(total)
+        previousRoll.append(runningTotal);
+        previousAmountView.text = String(runningTotal)
+        i = i + 1;
+
         
         //diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
         //diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
+    }
+    
+    func clear() {
+        
+        previousRoll = [0];
+        i = 0;
+        totalAmountView.text = String(0)
+        previousAmountView.text = String(0)
+        
     }
     
 }
