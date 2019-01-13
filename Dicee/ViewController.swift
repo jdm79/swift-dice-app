aeced0ed686cbd60defb641b5adbb6afb49280bd
jdm79 <james.malvern@gmail.com>
2019-01-13 01:12:57 +0000

    refactor: makes a new func rollsTracker which tallies number of rolls and running total of rolls

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -28,68 +28,55 @@ class ViewController: UIViewController {
         // this is a bit like componentDidMount in React
         // only gets triggered when the view loads up
         super.viewDidLoad()
-        
         defaultDiceImages()
-
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
         updateDiceImages()
-
     }
     
     @IBAction func clearButtonPressed(_ sender: UIButton) {
-    
         clear()
-    
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        
         updateDiceImages()
-        
     }
-    // this is exported and used in viewDidLoad()
-    func updateDiceImages() {
-        
-        // change range to 1 ... 6 if using the dynamic image path below
+    
+    func randomNumGenerator() {
         randomDiceIndex1 = Int.random(in: 0 ... 5);
         randomDiceIndex2 = Int.random(in: 0 ... 5);
         total = (randomDiceIndex1 + randomDiceIndex2) + 2;
         runningTotal = total + previousRoll[i];
-            
-        print(i)
-        
-        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    func rollsTracker() {
         totalRollAmountView.text = String(total)
         previousRoll.append(runningTotal);
         totalRollsView.text = String(i + 1)
         previousAmountView.text = String(runningTotal)
-
         i = i + 1;
-
-        
-        //diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
-        //diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
+    }
+    
+    func updateDiceImages() {
+        randomNumGenerator()
+        rollsTracker()
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
     func clear() {
-        
         previousRoll = [0];
         i = 0;
         totalRollAmountView.text = String(0)
         previousAmountView.text = String(0)
         totalRollsView.text = String(0)
         defaultDiceImages()
-        
     }
     
     func defaultDiceImages() {
         diceImageView1.image = UIImage(named: "dice\(7)")
         diceImageView2.image = UIImage(named: "dice\(7)")
     }
-    
 }
 
